c801e93d317cb4b8949874374c6fe1910a0476f9
Little-Motivated <164312772+Little-Motivated@users.noreply.github.com>
2024-10-17 17:51:39 +0300

    Init

---
+++ Scenes/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction = Input.get_vector("Left", "Right", "Up", "Down")
+	velocity = direction * 300
+	move_and_slide()
+	
